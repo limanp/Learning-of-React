c8e51ed66bb4531c0fce69ea820148a9ba8a9079
limanp <pasha.liman@ukr.net>
2022-09-09 19:45:07 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/d/db/Zeronet_logo.png' 
+            aria-hidden alt='image'/>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+    <div>
+      <img src='https://static5.depositphotos.com/1037262/521/i/450/depositphotos_5215941-stock-photo-road-panorama.jpg' 
+      aria-hidden alt='image'/>
+    </div>
+    <div>
+      ava + description
+    </div>
+    <div>
+      my post
+      <div>
+        new post
+      </div>
+      <div>
+        <div>
+          post 1
+        </div>
+        <div>
+          post 2
+        </div>
+      </div>
+    </div>
+  </div>
+}
+
+export default Profile;
