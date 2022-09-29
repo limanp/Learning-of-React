4f7563ee70f0f76757176949aee23f602860566a
limanp <pasha.liman@ukr.net>
2022-09-29 10:40:07 +0300

    lesson 51 [Users API]

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,43 +1,21 @@
+import axios from "axios";
 import React from "react";
 import style from "./Users.module.css";
+import userPhoto from "../../assets/images/userImage.png";
+
 const Users = (props) => {
   if (props.users.length === 0) {
-    props.setUsers([
-      {
-        id: 1,
-        photoUrl:
-          "https://i.pinimg.com/736x/de/a0/f3/dea0f3b7f480b1151c08db4a402a43b9.jpg",
-        followed: true,
-        fullName: "Dmitry K.",
-        status: "I am looking for a Job right now...",
-        location: { city: "Minsk", country: "Belarus" },
-      },
-      {
-        id: 2,
-        photoUrl:
-          "https://i.pinimg.com/736x/de/a0/f3/dea0f3b7f480b1151c08db4a402a43b9.jpg",
-        followed: false,
-        fullName: "Sergey S.",
-        status: "I like football!!!",
-        location: { city: "Kiev", country: "Ukraine" },
-      },
-      {
-        id: 3,
-        photoUrl:
-          "https://i.pinimg.com/736x/de/a0/f3/dea0f3b7f480b1151c08db4a402a43b9.jpg",
-        followed: true,
-        fullName: "Svetlana D.",
-        status: "I am so pretty",
-        location: { city: "Minsk", country: "Belarus" },
-      },
-    ]);
+    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+      debugger;
+      props.setUsers(response.data.items);
+    });
   }
   
   return (
     <div> {props.users.map(u => 
     <div className={style.user} key={u.id}>
         <div>
-            <div className={style.avatar}><img  src={u.photoUrl}/></div>
+            <div className={style.avatar}><img  src={u.photos.small != null ? u.photos.small : userPhoto}/></div>
             <div>
                 {u.followed
                 ? <button onClick={() => { props.unfollow(u.id) }}>follow</button> 
@@ -46,12 +24,12 @@ const Users = (props) => {
         </div>
         <div className={style.description}>
             <div>
-                <div>{u.fullName}</div>
+                <div>{u.name}</div>
                 <div>{u.status}</div>
             </div>
             <div>
-                <div>{u.location.country}</div>
-                <div>{u.location.city}</div>
+                <div>{"u.location.country"}</div>
+                <div>{"u.location.city"}</div>
             </div>
         </div>
             
