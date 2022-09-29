44853a7f56066e7b8b76cc32b7048393314b62d3
limanp <pasha.liman@ukr.net>
2022-09-29 17:23:56 +0300

    lesson 55 [pagination]

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,17 +1,22 @@
-import { followAC, unfollowAC, setUsersAC } from "./../../redux/users-reducer";
+import { followAC, unfollowAC, setUsersAC, setCurrentPage, setTotalUsersCount } from "./../../redux/users-reducer";
 import Users from "./Users";
 import { connect } from "react-redux";
 
 let mapStateToProps = (state) => {
     return {
-        users: state.usersPage.users
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage
     }
 }
 let mapDispatchToProps = (dispatch) => {
     return {
         follow: (usersId) => dispatch(followAC(usersId)),
         unfollow: (usersId) => dispatch(unfollowAC(usersId)),
-        setUsers: (users) => dispatch(setUsersAC(users))
+        setUsers: (users) => dispatch(setUsersAC(users)),
+        setCurrentPage: (currentPage) => dispatch(setCurrentPage(currentPage)),
+        setTotalUsersCount: (totalUsersCount) => dispatch(setTotalUsersCount(totalUsersCount))
     }
 }
 
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,11 +1,19 @@
 const FOLLOW = "FOLLOW";
 const UNFOLLOW = "UNFOLLOW";
 const SET_USERS = "SET_USERS";
+const SET_CURRENT_PAGE = "SET_CURRENT_PAGE";
+const SET_TOTAL_USERS_COUNT = "SET_TOTAL_USERS_COUNT";
+// ^ Константы для Action creators ^
+//--------------------------------------------------------
 
 let initialState = {
   users: [],
+  pageSize: 5,
+  totalUsersCount: 0,
+  currentPage: 1
 };
 
+// Reducer \/ \/
 export const usersReducer = (state = initialState, action) => {
   switch (action.type) {
     case FOLLOW:
@@ -19,18 +27,27 @@ export const usersReducer = (state = initialState, action) => {
       return {
         ...state, 
         users: state.users.map(u => {
-            return u.id === action.userId ? {...u, followed: false} : u;
+          return u.id === action.userId ? {...u, followed: false} : u;
         }),
       }
     case SET_USERS: 
-        return {...state, users: [...state.users, ...action.users]};
+      //return {...state, users: [...state.users, ...action.users]}; -- Добавить в конец массива
+      return {...state, users: action.users} // -- Перезаписать массив
+    case SET_CURRENT_PAGE: 
+       return {...state, currentPage: action.currentPage}
+    case SET_TOTAL_USERS_COUNT: 
+      return {...state, totalUsersCount: action.totalCount}
     default:
-        return state;
+      return state;
   }
 };
 
+// Action creator-ры 
 export const followAC = (userId) => ({ type: FOLLOW, userId: userId });
 export const unfollowAC = (userId) => ({ type: UNFOLLOW, userId: userId });
 export const setUsersAC = (users) => ({type: SET_USERS, users});
+export const setCurrentPage = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage});
+export const setTotalUsersCount = (totalCount) => ({type: SET_TOTAL_USERS_COUNT, totalCount});
+
 
 export default usersReducer;
