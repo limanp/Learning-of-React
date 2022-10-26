5a51dc2e8617e0b7729681b9fe676a0006838d0d
limanp <pasha.liman@ukr.net>
2022-10-26 17:53:02 +0300

    create method followUser and unfollowUser in usersAPI

--- src/api/api.js
+++ src/api/api.js
@@ -12,6 +12,14 @@ export const usersAPI = {
     getUser (currentPage, pageSize) {
         return instance.get(`users?page=${currentPage}&count=${pageSize}`)
         .then(response => response.data);
+    },
+    followUser (userId) {
+        return instance.post(`follow/${userId}`)
+        .then(response => response.data)
+    },
+    unfollowUser (userId) {
+        return instance.delete(`follow/${userId}`)
+        .then(response => response.data);
     }
 }
 
